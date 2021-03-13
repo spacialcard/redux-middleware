5cf8da28586247c38f312de59a2b5b17c8dea957
김형대 <>
2021-03-13 19:50:28 +0900

    ver3

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,12 @@
 import {combineReducers} from 'redux';
 import counter, { counterSage } from './counter';
-import posts from './posts';
+import posts, { postsSaga } from './posts';
 import { all } from 'redux-saga/effects';
 
 
 const rootReducer = combineReducers({counter ,posts});
 export function* rootSaga(){
-    yield all([counterSage()]);
+    yield all([counterSage(),postsSaga()]);
 }
 
 export default rootReducer;
